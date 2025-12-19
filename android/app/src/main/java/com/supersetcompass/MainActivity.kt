0216c0beff012ba3b0aae5ad08b7289cadc9c05f
olya <olya0665643972@gmail.com>
2025-12-19 23:16:38 +0200

    init

--- android/app/src/main/java/com/supersetcompass/MainActivity.kt
+++ android/app/src/main/java/com/supersetcompass/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.culture.compass.mercury
+package com.supersetcompass
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/supersetcompass/MainApplication.kt
+++ android/app/src/main/java/com/supersetcompass/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.culture.compass.mercury
+package com.supersetcompass
 
 import android.app.Application
 import com.facebook.react.PackageList
